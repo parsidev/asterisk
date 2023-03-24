4c0384c03dc6c299049878e0c21aeabf4f01b1ab
Mohammad Reza <programmer.net2009@live.com>
2023-03-24 14:23:35 +0330

    update

--- ami/option.go
+++ ami/option.go
@@ -32,10 +32,45 @@ type ConnectOptions struct {
 }
 type ConnectOption func(c *ConnectOptions) error
 
+func WithCustomDialer(dialer CustomDialer) ConnectOption {
+	return func(c *ConnectOptions) error {
+		c.Dialer = dialer
+		return nil
+	}
+}
+
+func WithOnConnectError(handler ConnErrHandler) ConnectOption {
+	return func(c *ConnectOptions) error {
+		c.OnConnectErr = handler
+		return nil
+	}
+}
+
+func WithOnConnected(handler ConnHandler) ConnectOption {
+	return func(c *ConnectOptions) error {
+		c.OnConnected = handler
+		return nil
+	}
+}
+
 func WithAuth(username string, secret string) ConnectOption {
 	return func(c *ConnectOptions) error {
 		c.Username = username
 		c.Secret = secret
+		return nil
+	}
+}
+
+func WithAllowReconnect() ConnectOption {
+	return func(c *ConnectOptions) error {
+		c.AllowReconnect = true
+		return nil
+	}
+}
+
+func WithTimeout(timeout time.Duration) ConnectOption {
+	return func(c *ConnectOptions) error {
+		c.Timeout = timeout
 		return nil
 	}
 }
@@ -46,6 +81,13 @@ func WithSubscribe(cb SubscribeFunc, opts ...SubscribeOption) ConnectOption {
 			sub  SubscribeFunc
 			opts []SubscribeOption
 		}{sub: cb, opts: opts})
+		return nil
+	}
+}
+
+func WithContext(ctx context.Context) ConnectOption {
+	return func(c *ConnectOptions) error {
+		c.Context = ctx
 		return nil
 	}
 }
